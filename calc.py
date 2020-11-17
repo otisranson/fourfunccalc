db678f149fd8b55dd0d137c3987e5ee2fda5ec63
Otis Ranson <otisranson@gmail.com>
2020-11-16 22:28:11 -0500

    Added test cases

--- calc.py
+++ calc.py
@@ -1,29 +1,17 @@
-import pdb, sys
-
 def add(x, y):
-    print(float(x) + float(y))
+    """Add function"""
+    return x + y
 
-def sub(x, y):
-    print(float(x) - float(y))
+def subtract(x, y):
+    """Subtract function"""
+    return x - y
 
-def mul(x, y):
-    print(float(x) * float(y))
+def multiply(x, y):
+    """Multiply function"""
+    return x * y
 
-def div(x, y):
-    print(float(x) / float(y))
-
-def main(argv):
-    if (argv[0] == 'add'):
-        add(argv[1],argv[2])
-    elif (argv[0] == 'sub'):
-        sub(argv[1],argv[2])
-    elif (argv[0] == 'div'):
-        div(argv[1],argv[2])
-    elif (argv[0] == 'mul'):
-        mul(argv[1],argv[2])
-    else:
-        print("Use add/sub/div/mul to do a calculation. I'm not that advanced.")
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+def divide(x, y):
+    """Division function"""
+    if y == 0:
+        raise ValueError('Cannot divide by zero!')
+    return x / y
