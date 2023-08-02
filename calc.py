c05ac4c2c3cdf45fdc4de2efa72b15b2e3895bca
Otis Ranson <otisranson@gmail.com>
2023-08-01 20:08:02 -0400

    Updaing code formatting

--- calc.py
+++ calc.py
@@ -2,13 +2,16 @@ def add(x, y):
     """Add function"""
     return x + y
 
+
 def subtract(x, y):
     """Subtract function"""
     return x - y
 
+
 def multiply(x, y):
     """Multiply function"""
     return x * y
+
 
 def divide(x, y):
     """Division function"""
